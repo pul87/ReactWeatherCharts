ad15bbb86b87d34a4c49eb33f2471e74692b4f21
Paolo Basso <paolo.basso87@gmail.com>
2017-01-22 16:56:49 +0100

    Rimosso conole.log

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -5,7 +5,6 @@ export default function(state=[], action) {
 
   switch(action.type) {
     case FETCH_WEATHER: {
-      console.log(action.payload);
       return [ action.payload.data, ...state ];
     }
 
