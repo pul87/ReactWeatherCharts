545c150793d107bcb4231f62b182a91c58be4563
Paolo Basso <paolo.basso87@gmail.com>
2017-01-22 16:59:26 +0100

    refactoring

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,8 @@
 // libraries
 import axios from 'axios';
 
-const API_KEY = '6c56b8d5f68ea40a65fe9b8e556cd0c1';
-const ROOT_URL     = `http://api.openweathermap.org/data/2.5/forecast?appId=${API_KEY}`;
+const API_KEY  = '6c56b8d5f68ea40a65fe9b8e556cd0c1';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appId=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
